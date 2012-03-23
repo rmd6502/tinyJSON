14631fb1094c1f35f74b3d55d87de975409d7e62
Robert Diamond <robert.diamond@gmail.com>
2012-03-22 20:02:23 -0400

    Initial Commit

---
+++ tinyJSON/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  tinyJSON
+//
+//  Created by Robert Diamond on 3/22/12.
+//  Copyright (c) 2012 America Online. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
